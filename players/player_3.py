1511f86bf8eb279ddc11d4d5f7b03bc4107ed12d
Landry Valetoux <58473063+landry42@users.noreply.github.com>
2022-01-14 13:36:14 +0100

    Initialisation groupe Landry - William
    Coopération intelligente

--- players/player_3.py
+++ players/player_3.py
@@ -5,7 +5,11 @@ class Player:
     turn_gain: int = 0
     cooperate_count: int = 0
     cheat_count: int = 0
-    name: str = "Player 3"
+    name: str = "Landry et William"
 
     def play(turn_index: int, history: list[Turn]) -> Action:
-        return Action.COOPERATE
+        if len(history) == 0:
+            return Action.COOPERATE
+        else:
+            return history[-1]["opponent_action"]
+        
